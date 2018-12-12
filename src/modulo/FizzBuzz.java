eafacd137ffb80216002746f9197bf0be7765ac7
peter <League@AdministorsiMac.attlocal.net>
2018-12-11 19:55:36 -0800

    Module3

--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -26,9 +26,17 @@ package modulo;
 **/
 
 public class FizzBuzz {
-	for (int i = 0; i < 21; i++) {
-		if () {
-			
+	public static void main(String[] args) {
+		for (int i = 1; i < 20; i++) {
+			if (i%3 == 0) {
+				System.out.print("Fizz ");
+			}else if (i%5 == 0) {
+				System.out.print("Buzz ");
+			}else if (i%3 == 0 && i%3 == 0) {
+				System.out.print("FizzBuzz ");
+			}else {
+				System.out.print(i + " ");
+			}
 		}
 	}
 }
